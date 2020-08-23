fb7111c6499482d919f05b202657e934fd5b089f
Dibwe243 <dibwe.kalangu@umuzi.org>
2020-08-23 05:10:58 +0200

    commit

--- server/express.js
+++ server/express.js
@@ -5,28 +5,14 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
-// modules for server side rendering
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import MainRouter from './../client/MainRouter'
-import { StaticRouter } from 'react-router-dom'
 
-import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
-import theme from './../client/theme'
-//end
-
-//comment out before building for production
-import devBundle from './devBundle'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
-//comment out before building for production
-devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
@@ -44,27 +30,7 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use('/', userRoutes)
 app.use('/', authRoutes)
 
-app.get('*', (req, res) => {
-  const sheets = new ServerStyleSheets()
-  const context = {}
-  const markup = ReactDOMServer.renderToString(
-    sheets.collect(
-          <StaticRouter location={req.url} context={context}>
-            <ThemeProvider theme={theme}>
-              <MainRouter />
-            </ThemeProvider>
-          </StaticRouter>
-        )
-    )
-    if (context.url) {
-      return res.redirect(303, context.url)
-    }
-    const css = sheets.toString()
-    res.status(200).send(Template({
-      markup: markup,
-      css: css
-    }))
-})
+
 
 // Catch unauthorised errors
 app.use((err, req, res, next) => {
